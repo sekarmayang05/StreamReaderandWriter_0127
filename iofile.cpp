9627a61bef1d502e842d4c0c6574d426a0c6a4aa
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 18:03:49 +0700

    mnembahkan perulangan while dan menampilkan di sisi lalu menutup file

--- iofile.cpp
+++ iofile.cpp
@@ -27,5 +27,17 @@ int main () {
     // menunjuk ke sebuah file 
     infile.open("contoh.txt");
     cout << endl << ">= Membuka dan membaca file " << endl;
-
+    //jika file ada maka 
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sisi 
+            cout << baris << '\n';
+        }
+        //tutup file tersebt setelah selesai
+        infile.close();
+    }
+    
 }
