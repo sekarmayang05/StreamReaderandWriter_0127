f60029ea5e46a9d864c640ff46393a7b49c775a1
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 17:42:06 +0700

    membuka file dalam mode menulis

---
+++ iofile.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    string  baris;
+    // membuka file dalam mode menulis. :
+    ofstream outfile;
+    //menunjuk ke sebuah file 
+    outfile.open("contohfile.txt");
+    cout <<  ">= Menulis file, \'q' untuk keluar" << endl;
+    // unlimited loo untuk menulis
+    while (true){
+        cout << "-";
+        // membuat setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file 
+    infile.open("contoh.txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka 
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sisi 
+            cout << baris << '\n';
+        }
+        //tutup file tersebt setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan isi
+    else cout << "unable to open file";
+    return 0;
+}
