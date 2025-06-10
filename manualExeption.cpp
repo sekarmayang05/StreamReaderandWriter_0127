ef0e6120c69938b2c2cfbcc9f8ee74a1100a81b4
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 19:04:08 +0700

    Ngetes try-catch, lempar double tapi nangkep int, jadi masuk ke default catch

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -8,5 +8,13 @@ int main ()
         throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak dieksekusi" << endl;
     }
-   
+    catch  (int a){
+        // blok ini akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
