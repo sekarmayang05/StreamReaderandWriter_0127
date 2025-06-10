fdee8b16487fc424820285f0e0e5b60cbc22d8ee
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 18:21:20 +0700

    membuka file dalam mode menulis dan menunjukkan ke sebuah nama file

--- iofileWithPath.cpp
+++ iofileWithPath.cpp
@@ -8,5 +8,12 @@ int main(){
     string NamaFile;
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
-
+    // membuka file dala mode menulis.
+    ofstream outfile;
+    // menunjukan ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file \'q\' untuk keluar" << endl;
+    // unlimited loop untuk menulis
+   
+    
 }
