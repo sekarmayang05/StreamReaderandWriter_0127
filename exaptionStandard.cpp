d70721622ba146116f5b569a2cd12c89cca1a050
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 18:51:24 +0700

    menampilkan awal program, memberi pesan array integer 3 elemen dan memanggil array elemen ke 5

--- exaptionStandard.cpp
+++ exaptionStandard.cpp
@@ -5,3 +5,14 @@
 // untuk obyek array yang akan kita gunakan 
 using namespace std;
 
+int main ()
+{
+    cout << "Awal program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+        //memanggil array  elemen ke 5
+    }
+  
+}
