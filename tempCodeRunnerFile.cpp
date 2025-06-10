0bcb0f3287d4313725ab814cc1233970cdfc0c67
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 17:58:25 +0700

    add loop input baris hingga memasukan ke "q" lalu menulis ke file

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,9 @@
-    outfile.open("contohfile.txt");
-    cout <<  ">= Menulis file, \'q' untuk keluar" << endl;
-    // unlimited loop untuk menulis
+    while (true){
+        cout << "-";
+        // membuat setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
