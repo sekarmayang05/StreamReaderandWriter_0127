2db18d0074659ce5b4c92912438a3c292a0b957a
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-10 18:05:27 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-    while (true){
-        cout << "-";
-        // membuat setiap karakter dalam satu baris
-        getline(cin, baris);
-        // loop akan berhenti jika anda memasukan karakter q
-        if (baris == "q") break;
-        //menulis dan memasukan nilai dari 'baris' ke dalam file 
-        outfile << baris << endl;
-    }
